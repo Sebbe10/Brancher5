c8fbbfeb4ba28c02fff79759b36c7205b241342b
Sebastian Bjelkstam <sebastian.bjelkstam@gmail.com>
2022-12-02 11:21:47 +0100

    ha

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -18,15 +18,11 @@ class Person {
 
 let myul = new Person("Hej");
 
-let myul2 = new Person("Tja");
-
-let myul3 = new Person("Hejsan");
-
 let names = 0;
 
 let ulTag = document.getElementById("myUL");
 
-let myuls = ["Hej", "Tja", "Hejsan"];
+let myuls = ["Hej"];
 
 for (let i = 0; i < myuls.length; i++) {
   console.log(myuls);
